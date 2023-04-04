8ebe102564c19221161502172a6f3388c0f65359
wonderboy1w3 <wonderboy1w3@gmail.com>
2023-04-04 06:20:33 +0500

    Models created and Connected to db

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using System.Text;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            string msg = "Hello world this is index page!\n";
+            return View("index", msg);
+        }
+        //[HttpGet("[controller]/welcomePage")]
+        public IActionResult Welcome(string name, int loopcount = 1)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < loopcount; i++)
+            {
+                sb.Append($"Hello {name}\n");
+            }
+            return View("Welcome", sb.ToString());
+        }
+    }
+}
---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
